af34f13d8c37d8f75024a224c2cf82b1a883ec9b
eunhyePark <eunhye0406@gmail.com>
2020-08-20 21:27:36 +0900

    Updates

---
+++ precache-manifest.230ce10650d50009d66a84fbbaac6191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4623ad6773cc3f4e7d4b422924d6c52d",
+    "url": "/kurepark/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7e1bda25bf92ddebdd91",
+    "url": "/kurepark/movie_app_2020/static/css/main.184a2473.chunk.css"
+  },
+  {
+    "revision": "22cb90122fd5507e6a66",
+    "url": "/kurepark/movie_app_2020/static/js/2.ff56ef60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kurepark/movie_app_2020/static/js/2.ff56ef60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1bda25bf92ddebdd91",
+    "url": "/kurepark/movie_app_2020/static/js/main.e9d031ae.chunk.js"
+  },
+  {
+    "revision": "0bb64171860d9e8e59c4",
+    "url": "/kurepark/movie_app_2020/static/js/runtime-main.b4763e75.js"
+  }
+]);
